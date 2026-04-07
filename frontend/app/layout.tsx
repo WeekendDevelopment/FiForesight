dbfb8582a94615b17862fe3f8cea230272f219ba
nithin554 <nithinneeraj60@gmail.com>
2026-04-07 21:34:58 +0200

    add newrelic tracing

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,10 @@ export const metadata: Metadata = {
   description: "AI-Based Financial Forecasting",
 };
 
+const nrScript = process.env.APP_ENV === 'live'
+  ? '/newrelic.live.js'
+  : '/newrelic.preview.js'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,6 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+          id="newrelic"
+          strategy="beforeInteractive"
+          src={nrScript}
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
