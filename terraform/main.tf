9ad8ece426c52dfb4cd1dc33c2a88371f5e6f0ca
Sai <saikrishnacr2012@gmail.com>
2026-04-22 19:44:15 +0100

    fix: set boot_volume_size_in_gbs=50 to meet OCI provider minimum

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,8 +103,9 @@ resource "oci_core_instance" "vm" {
   }
 
   source_details {
-    source_type = "image"
-    source_id   = data.oci_core_images.ubuntu.images[0].id
+    source_type             = "image"
+    source_id               = data.oci_core_images.ubuntu.images[0].id
+    boot_volume_size_in_gbs = 50
   }
 
   metadata = {
